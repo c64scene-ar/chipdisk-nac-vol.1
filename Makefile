385039547a77cee3f8ec8349155fcfae4e720a60
Ricardo Quesada <ricardoquesada@gmail.com>
2016-02-16 12:07:45 -0800

    cursos with joy enabled

--- Makefile
+++ Makefile
@@ -9,8 +9,8 @@ all: chipdisk-dev
 SRC = src/chipdisk.s
 
 chipdisk-dev: ${SRC}
-	cl65 -o bin/$@.prg -u __EXEHDR__ -t c64 -C chipdisk.cfg $^
-	$(X64) -moncommands $@.sym bin/$@.prg
+	cl65 -d -g -Ln bin/$@.sym -o bin/$@.prg -u __EXEHDR__ -t c64 -C chipdisk.cfg $^
+	$(X64) -moncommands bin/$@.sym bin/$@.prg
 
 clean:
-	rm -f src/*.o *.sym bin/*.prg
+	rm -f src/*.o bin/*.sym bin/*.prg
--- src/chipdisk.s
+++ src/chipdisk.s
@@ -59,7 +59,7 @@ BITMAP_ADDR = $2000 + 8 * 40 * 12
         ldy #>irq
         stx $fffe
         sty $ffff
-        
+
         lda #$00
         sta $d012
 
@@ -70,9 +70,15 @@ BITMAP_ADDR = $2000 + 8 * 40 * 12
         cli
 
 main_loop:
-        lda sync_raster
+        lda sync_raster                 ; raster triggered ?
         beq :+
         jsr do_raster_anims
+
+        jsr read_joy2
+        lda $dc00
+        eor #$7f
+        beq :+
+        jsr process_joy2
 :
         jmp main_loop
 
@@ -94,16 +100,135 @@ main_loop:
 .endproc
 
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; read_joy2
+; Routine taken from here:
+; http://codebase64.org/doku.php?id=base:joystick_input_handling&s[]=joystick
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+.proc read_joy2
+
+        lda $dc00                       ; get input from port 2 only
+        ldy #0                          ; this routine reads and decodes the
+        ldx #0                          ; joystick/firebutton input data in
+        lsr                             ; the accumulator. this least significant
+        bcs djr0                        ; 5 bits contain the switch closure
+        dey                             ; information. if a switch is closed then it
+djr0:   lsr                             ; produces a zero bit. if a switch is open then
+        bcs djr1                        ; it produces a one bit. The joystick dir-
+        iny                             ; ections are right, left, forward, backward
+djr1:   lsr                             ; bit3=right, bit2=left, bit1=backward,
+        bcs djr2                        ; bit0=forward and bit4=fire button.
+        dex                             ; at rts time dx and dy contain 2's compliment
+djr2:   lsr                             ; direction numbers i.e. $ff=-1, $00=0, $01=1.
+        bcs djr3                        ; dx=1 (move right), dx=-1 (move left),
+        inx                             ; dx=0 (no x change). dy=-1 (move up screen),
+djr3:   lsr                             ; dy=0 (move down screen), dy=0 (no y change).
+                                        ; the forward joystick position corresponds
+                                        ; to move up the screen and the backward
+                                        ; position to move down screen.
+                                        ;
+                                        ; at rts time the carry flag contains the fire
+                                        ; button state. if c=1 then button not pressed.
+                                        ; if c=0 then pressed.
+        rts
+.endproc
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+; process_joy2
+; X = -1, 0, or 1
+; Y = -1, 0, or 1
+; C = button pressed if Carry Clear
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
+.proc process_joy2
+        cpx #0
+        beq test_y                      ; skip if no changes in Y
+
+        clc                             ; set new sprite X position
+        txa
+        adc $d000 + 6 * 2
+        sta $d000 + 6 * 2
+        sta $d000 + 7 * 2
+
+        pha                             ; save in tmp
+
+        bcs test_addition
+
+        txa                             ; Carry Clear and substracting ?, then change MSB
+        bmi change_MSB
+        bpl check_x_bounds
+
+test_addition:
+        txa
+        bpl change_MSB                  ; Carry Set and adding ?, then change MSB
+        bmi check_x_bounds
+
+change_MSB:
+        lda $d010                       ; sprite x MSB
+        eor #%11000000
+        sta $d010
+
+check_x_bounds:
+        pla                             ; get A from tmp
+        tax
+
+        lda $d010
+        and #%10000000
+        bne x_upper
+
+        txa
+        cmp #24                         ; lower bounds: x < 24 and LSB
+        bcs test_y
+        lda #24
+        sta $d000 + 6 * 2
+        sta $d000 + 7 * 2
+        bne test_y
+
+x_upper:
+        txa
+        cmp #84                         ; lower bounds: x > 80 and MSB
+        bcc test_y
+        lda #84
+        sta $d000 + 6 * 2
+        sta $d000 + 7 * 2
+
+
+test_y:
+        cpy #0                          ; no Y movment... quick end
+        beq end
+
+        clc
+        tya
+        adc $d000 + 6 * 2 + 1           ; set new sprite Y position
+        tay                             ; save A in tmp
+
+        cmp #50                         ; don't go lower than 50
+        bcs :+
+        lda #50                         ; if lower, set 50 as the minimum
+        bne set_y                       ; and jump
+
+        tya                             ; restore A from tmp
+:       cmp #245                        ; above 200 ?
+        bcc set_y
+        lda #245                        ; set 200 as the maximum
+
+set_y:
+        sta $d000 + 6 * 2 + 1
+        sta $d000 + 7 * 2 + 1
+end:
+        rts
+
+.endproc
+
+;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 ; do_anim_cassette
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 .proc do_anim_cassette
-        ldx $63f8
+        ldx $63f8                       ; sprite pointer for sprite #0
         inx
         cpx #(144 + 16)
         bne :+
         ldx #144
-:       stx $63f8
-        stx $63f9
+:       stx $63f8                       ; turning wheel sprite pointer #0
+        stx $63f9                       ; turning wheel sprite pointer #1
         rts
 .endproc
 
@@ -111,7 +236,7 @@ main_loop:
 ; init_sprites
 ;=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-;
 .proc init_sprites
-        lda #03;                        ; enable sprite 0,1
+        lda #%11000011                  ; enable sprites
         sta VIC_SPR_ENA
 
         lda #0
@@ -140,16 +265,16 @@ l1:
         rts
 
 sprites_x_pos:
-        .byte 192, 136, 0, 0, 0, 0, 0, 0
+        .byte 192, 136, 0, 0, 0, 0, 150, 150
 
 sprites_y_pos:
-        .byte 126, 98, 0, 0, 0, 0, 0, 0
+        .byte 126, 98, 0, 0, 0, 0, 150, 150
 
 sprites_color:
-        .byte 12, 12, 0, 0, 0, 0, 0, 0
+        .byte 12, 12, 0, 0, 0, 0, 0, 1
 
 sprites_pointer:
-        .byte 144, 144, 0, 0, 0, 0, 0, 0
+        .byte 144, 144, 0, 0, 0, 0, 207, 206
 
 .endproc
 
